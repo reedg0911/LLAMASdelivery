a1e395d4c69d39552adfbdd4c3d6fcdaa3a1fc38
nirbhayvig <nirbhayvig43@gmail.com>
2023-03-07 18:21:50 -0600

    init

---
+++ HTML/js/script.js
@@ -0,0 +1,159 @@
+(function($) {
+    "use strict";
+
+    // Windows load
+
+    $(window).on("load", function() {
+
+        // Site loader 
+
+        $(".loader-inner").fadeOut();
+        $(".loader").delay(200).fadeOut("slow");
+
+    });
+
+
+    // Scroll to
+
+    $('a.scroll').smoothScroll({
+        speed: 800,
+        offset: -52
+    });
+
+
+
+    // Site navigation setup
+
+    var header = $('.header'),
+        pos = header.offset();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > pos.top + 500 && header.hasClass('default')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('default').addClass('switched-header').fadeIn(200);
+            });
+        } else if ($(this).scrollTop() <= pos.top + 500 && header.hasClass('switched-header')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('switched-header').addClass('default').fadeIn(100);
+            });
+        }
+    });
+
+
+
+
+    // Hero resize
+
+    function mainHeroResize() {
+        $(".main-slider .slides li").css('height', $(window).height());
+    }
+
+    $(function() {
+            mainHeroResize()
+        }),
+        $(window).resize(function() {
+            mainHeroResize()
+        });
+
+
+
+
+    // Slider
+
+    $('.main-slider').flexslider({
+        animation: "fade",
+        slideshow: true,
+        directionNav: true,
+        controlNav: true,
+        pauseOnAction: false,
+        animationSpeed: 500
+    });
+
+
+    $('.review-slider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        directionNav: true,
+        controlNav: false,
+        pauseOnAction: false,
+        animationSpeed: 500
+    });
+
+
+
+
+    // Mobile menu
+
+    var mobileBtn = $('.mobile-but');
+    var nav = $('.main-nav ul');
+    var navHeight = nav.height();
+
+    $(mobileBtn).on("click", function() {
+        $(".toggle-mobile-but").toggleClass("active");
+        nav.slideToggle();
+        $('.main-nav li a').addClass('mobile');
+        return false;
+
+
+    });
+
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && nav.is(':hidden')) {
+            nav.removeAttr('style');
+            $('.main-nav li a').removeClass('mobile');
+        }
+
+    });
+
+    $('.main-nav li a').on("click", function() {
+        if ($(this).hasClass('mobile')) {
+            nav.slideToggle();
+            $(".toggle-mobile-but").toggleClass("active");
+        }
+
+    });
+
+
+
+    // Append images as css background
+
+    $('.background-img').each(function() {
+        var path = $(this).children('img').attr('src');
+        $(this).css('background-image', 'url("' + path + '")').css('background-position', 'initial');
+    });
+
+
+
+
+    // Tabbed content 
+
+    $(".block-tabs li").on("click", function() {
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum + 1;
+            $(".block-tabs li.active").removeClass("active");
+            $(this).addClass("active");
+            $(".block-tab li.active").removeClass("active");
+            $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
+        }
+    });
+
+
+
+
+    // Instagram feed setup
+    var instaFeed = new Instafeed({
+        get: 'user',
+        userId: '6366233572',
+        accessToken: '6366233572.66a2324.a32ba61504e041da93f98ed3905039fb',
+        limit: 10,
+        resolution: 'standard_resolution',
+        template: '<li><a href="{{link}}"><img src="{{image}}"/></a></li>'
+    });
+    instaFeed.run();
+
+
+
+
+})(jQuery);
